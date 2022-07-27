11561efda41df92ee78c5c5c507d320e8dc74fd6
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-07-27 18:31:36 -0500

    .

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const toolCategories = ['languages', 'package-managers', 'frontend', 'styling', 'backend', 'deploying', 'dev-tools'];
-
-toolCategories.forEach(category => {
-  document.getElementById(`tools-${category}-toggle`).onclick = async () => {
-    const content = document.getElementById(`tools-${category}-content`);
-
-    if (content.style.display === 'block') {
-      content.style.display = 'none';
-      return;
-    }
-    if (content.style.display === 'none') {
-      content.style.display = 'block';
-      return;
-    }
-
-    content.style.display = 'none';
-  };
-});
+window.onload = () => {
+  console.log("%c        __ _.--..--._ _\r\n     .-' _/   _/\\_   \\_'-.\r\n    |__ /   _/\\__/\\_   \\__|\r\n       |___/\\_\\__/  \\___|\r\n              \\__/\r\n              \\__/\r\n               \\__/\r\n                \\__/\r\n             ____\\__/___\r\n       . - '             ' -.\r\n      /                      \\\r\n~~~~~~~  ~~~~~ ~~~~~  ~~~ ~~~  ~~~~~\r\n      ~~~   ~~~~~   ~~~   ~~ ~  ~ ~ ~\n             emberwave", 'color: #8229c6');
+};
